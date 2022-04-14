272b563d7aa6b7694906ed5a66e564aadb897e97
aleksbobic <bobic.aleksandar92@gmail.com>
2022-04-14 12:23:03 +0200

    Add list of datasets to homepage

--- app/src/pages/home/Home.page.jsx
+++ app/src/pages/home/Home.page.jsx
@@ -1,17 +1,22 @@
+import { SettingsIcon } from '@chakra-ui/icons';
 import {
     Box,
     Center,
     Container,
+    Flex,
     Heading,
     HStack,
+    IconButton,
     Image,
     Link,
+    SimpleGrid,
     Text,
+    Tooltip,
     useColorMode
 } from '@chakra-ui/react';
 import SearchBarComponent from 'components/feature/searchbar/SearchBar.component';
 import FileUploadModalComponent from 'components/interface/fileuploadmodal/FileUploadModal.component';
-import { FileAdd } from 'css.gg';
+import { ArrowRight, FileAdd, ShapeHexagon, Toolbox, TrashEmpty } from 'css.gg';
 import logo from 'images/logo.png';
 import logodark from 'images/logodark.png';
 import logolight from 'images/logolight.png';
@@ -153,6 +158,80 @@ function HomePage() {
         </Container>
     );
 
+    const renderDatasetGrid = () => {
+        return (
+            <SimpleGrid
+                columns={2}
+                spacing="10px"
+                marginTop="40px"
+                backgroundColor="blackAlpha.300"
+                padding="10px"
+                borderRadius="12px"
+                maxHeight="170px"
+                overflowY="scroll"
+            >
+                {store.search.datasets.map((dataset, index) => (
+                    <Flex
+                        backgroundColor="whiteAlpha.50"
+                        borderRadius="8px"
+                        height="40px"
+                        justifyContent="center"
+                        alignItems="center"
+                        gap="5px"
+                        paddingLeft="5px"
+                        paddingRight="5px"
+                        key={`dataset_list_${dataset}`}
+                        opacity="0.8"
+                        transition="all 0.1s ease-in-out"
+                        _hover={{ opacity: '1' }}
+                    >
+                        <Heading
+                            flexGrow="1"
+                            size="xs"
+                            textAlign="left"
+                            paddingLeft="10px"
+                        >
+                            {dataset}
+                        </Heading>
+                        <Tooltip label={`Delete ${dataset}`}>
+                            <IconButton
+                                flexGrow="0"
+                                size="sm"
+                                variant="ghost"
+                                icon={
+                                    <TrashEmpty
+                                        style={{
+                                            '--ggs': '0.7',
+                                            marginTop: '1px'
+                                        }}
+                                    />
+                                }
+                            />
+                        </Tooltip>
+                        <Tooltip
+                            label={`Change default settings for  ${dataset}`}
+                        >
+                            <IconButton
+                                flexGrow="0"
+                                size="sm"
+                                variant="ghost"
+                                icon={<Toolbox style={{ '--ggs': '0.7' }} />}
+                            />
+                        </Tooltip>
+                        <Tooltip label={`Explore entire ${dataset}`}>
+                            <IconButton
+                                flexGrow="0"
+                                size="sm"
+                                variant="solid"
+                                icon={<ArrowRight style={{ '--ggs': '0.7' }} />}
+                            />
+                        </Tooltip>
+                    </Flex>
+                ))}
+            </SimpleGrid>
+        );
+    };
+
     return (
         store.search.datasets && (
             <Box
@@ -183,6 +262,7 @@ function HomePage() {
                     maxW="container.sm"
                 >
                     <SearchBarComponent style={{ marginTop: '0px' }} />
+                    {renderDatasetGrid()}
                     {renderFileUpload()}
                 </Container>
                 {renderFooter()}
