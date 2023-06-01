49de251d414a86a5bb17a6e8b6616d7a630a9a66
aleksbobic <bobic.aleksandar92@gmail.com>
2023-06-01 13:46:41 +0200

    Implement mongo search connector

--- server/app/services/search/mongo_search_connector.py
+++ server/app/services/search/mongo_search_connector.py
@@ -73,8 +73,9 @@ class MongoSearchConnector(BaseSearchConnector):
         return [name for name in self.database.list_collection_names()]
 
     def get_dataset_features(self, dataset_name: str) -> Union[dict, None]:
+        # get sample document and exclude _id field
         random_document = self.database[dataset_name].aggregate(
-            [{"$sample": {"size": 1}}]
+            [{"$sample": {"size": 1}}, {"$project": {"_id": 0}}]
         )
 
         if random_document:
@@ -91,7 +92,10 @@ class MongoSearchConnector(BaseSearchConnector):
     def get_full_dataset(self, dataset_name: str) -> pd.DataFrame:
         all_docs = list(self.database[dataset_name].find({}))
 
-        return pd.DataFrame(all_docs).rename(columns={"_id": "entry"})
+        all_docs = pd.DataFrame(all_docs)
+        all_docs["_id"] = all_docs["_id"].astype(str)
+
+        return all_docs.rename(columns={"_id": "entry"})
 
     def simple_search(
         self, dataset_name: str, query: str, features: List[str]
@@ -109,12 +113,108 @@ class MongoSearchConnector(BaseSearchConnector):
 
         return search_results.rename(columns={"_id": "entry"})
 
+    def __range_filter_to_dataframe(
+        self,
+        feature: str,
+        min: Union[int, float],
+        max: Union[int, float],
+        dataset_name: str,
+    ) -> pd.DataFrame:
+        """Run a range filter betwee values min and max on the provided index and feature and retrieve results as a dataframe"""
+        search_results = list(
+            self.database[dataset_name].find({feature: {"$gte": min, "$lte": max}}, {})
+        )
+
+        search_results = pd.DataFrame(search_results)
+        search_results["_id"] = search_results["_id"].astype(str)
+
+        return search_results.rename(columns={"_id": "entry"})
+
+    def __negated_search(
+        self, dataset_name: str, query: str, feature: str
+    ) -> pd.DataFrame:
+        search_results = list(
+            self.database[dataset_name].find({feature: {"$not": {"$regex": query}}}, {})
+        )
+        if not search_results:
+            return pd.DataFrame()
+
+        search_results = pd.DataFrame(search_results)
+        search_results["_id"] = search_results["_id"].astype(str)
+
+        return search_results.rename(columns={"_id": "entry"})
+
     def advanced_search(
         self, dataset_name: str, query: dict, features: List[str]
     ) -> pd.DataFrame:
-        return pd.DataFrame()
+        if "min" in query and "max" in query:
+            return self.__range_filter_to_dataframe(
+                query["feature"], float(query["min"]), float(query["max"]), dataset_name
+            )
+
+        if query["action"] == "get dataset":
+            return self.get_full_dataset(dataset_name)
+
+        if "query" not in query and "queries" not in query:
+            return self.simple_search(
+                dataset_name, query["keyphrase"], [query["feature"]]
+            )
+
+        if query["action"] == "connect":
+            if query["connector"] == "or":
+                query_dfs = [
+                    self.advanced_search(dataset_name, entry, features)
+                    for entry in query["queries"]
+                ]
+
+                merged_df = (
+                    pd.concat(query_dfs, ignore_index=True)
+                    .drop_duplicates(subset=["entry"])
+                    .reset_index(drop=True)
+                )
+
+                return merged_df
+
+            if query["connector"] == "and":
+                query_dfs = [
+                    self.advanced_search(dataset_name, entry, features)
+                    for entry in query["queries"]
+                ]
+
+                merged_df = query_dfs[0]
+                query_dfs = query_dfs[1:]
+
+                for entry_df in query_dfs:
+                    merged_df = pd.concat([merged_df, entry_df], ignore_index=True)
+                    merged_df = (
+                        merged_df[merged_df.duplicated(subset=["entry"])]
+                        .drop_duplicates(subset=["entry"])
+                        .reset_index(drop=True)
+                    )
+
+                return merged_df
+
+            return self.__negated_search(
+                dataset_name,
+                query["queries"][0]["keyphrase"],
+                query["queries"][0]["feature"],
+            )
+
+        return self.advanced_search(dataset_name, query["query"], features)
 
     def get_entries_by_id(
         self, dataset_name: str, entry_ids: List[str]
     ) -> pd.DataFrame:
-        return pd.DataFrame()
+        entry_ids_transformed = [ObjectId(entry_id) for entry_id in entry_ids]
+
+        search_results = list(
+            self.database[dataset_name].find({"_id": {"$in": entry_ids_transformed}})
+        )
+
+        if not search_results:
+            return pd.DataFrame()
+
+        search_results = pd.DataFrame(search_results)
+        search_results["_id"] = search_results["_id"].astype(str)
+
+        return search_results.rename(columns={"_id": "entry"})
